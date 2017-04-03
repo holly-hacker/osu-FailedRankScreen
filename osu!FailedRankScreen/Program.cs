4e7b4febfe1cc87c8b221d58be4aea4a74cdd48f
HoLLy <dario.db@hotmail.com>
2017-04-03 19:59:40 +0200

    Write code

--- osu!FailedRankScreen/Program.cs
+++ osu!FailedRankScreen/Program.cs
@@ -1,22 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace osu_FailedRankScreen
 {
-    static class Program
+    internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Debug.WriteLine("Trying to find osu! location");
+            Process[] pl = Process.GetProcessesByName("osu!");
+            if (pl.Length == 0) ExitError("osu! is not running!");
+            Process p = pl.First();
+            string path = p.MainModule.FileName;
+
+            Debug.WriteLine("Loading assembly");
+            Assembly ass = Assembly.LoadFrom(path);
+
+            Debug.WriteLine("Finding type");
+            Type t = ass.ExportedTypes.First(a => a.FullName == "osu.Helpers.InterProcessOsu");
+
+            Debug.WriteLine("Getting InterProcessOsu");
+            object ipo = Activator.GetObject(t, "ipc://osu!/loader");
+
+            Debug.WriteLine("Executing method");
+            t.GetMethod("ChangeMode").Invoke(ipo, new object[] { 7 });
+
+            MessageBox.Show("Done!");
         }
+
+        private static void ExitError(string message)
+        {
+            MessageBox.Show(message, "An error has occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(-1);
+        }
+
+        /*
+        public static void Unsafe() //in 3 lines :)
+        {
+            string p = Process.GetProcessesByName("osu!")[0].MainModule.FileName;
+            Type t = Assembly.LoadFrom(p).ExportedTypes.First(a => a.FullName == "osu.Helpers.InterProcessOsu");
+            t.GetMethod("ChangeMode").Invoke(Activator.GetObject(t, "ipc://osu!/loader"), new object[] { 7 });
+        }
+        */
     }
 }
